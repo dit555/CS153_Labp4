eee83aeda3772718907f02c61489ed144fc2d101
Dumitru Chiriac <dchir002@sledge.cs.ucr.edu>
2020-12-15 15:12:36 -0800

    part one is finsihed but potentially buggy, need to do part 2 to debug

--- shm.c
+++ shm.c
@@ -29,13 +29,78 @@ void shminit() {
 }
 
 int shm_open(int id, char **pointer) {
+	
+  	int i, j, sz, newsz;
+	int maxid;
+	char* mem;
+	uint a;
+	a = 0;
+  	initlock(&(shm_table.lock), "SHM lock");
+  	acquire(&(shm_table.lock));
+  	//find id if it exists
+	for (i = 0; i< 64; i++) {
+    		if(shm_table.shm_pages[i].id == id){
+			mappages(myproc()->pgdir, (char*)a , PGSIZE, V2P(shm_table.shm_pages[i].frame), PTE_W|PTE_U);
+			shm_table.shm_pages[i].refcnt++;
+			*pointer = (char*)a;
+   			myproc()->sz += PGSIZE;
+			release(&(shm_table.lock));
+			return 0;	
+		}
+  	}
+	
+	//find unused page and do the thing
+	for(i = 0; i < 64; i++){
+		if (shm_table.shm_pages[i].id == 0){
+			break;
+		}
+	}
+	
+	maxid = 0;
+	for(j = 0; j < 64; j++){	
+		//find larges id value then add 1 for new id
+		if (shm_table.shm_pages[j].id >= maxid)
+			maxid = shm_table.shm_pages[j].id;
+	}
 
-//you write this
+	maxid++; //new id 
+	sz = myproc()->sz;
+	newsz = sz - PGSIZE;
+	
+	if(newsz >= KERNBASE)
+    		return -1;
+  	
+	
 
-
-
-
-return 0; //added to remove compiler warning -- you should decide what to return
+	a = PGROUNDUP(sz);
+	for(; a < newsz; a += PGSIZE){
+    		mem = kalloc();
+    		if(mem == 0){
+      			cprintf("allocuvm out of memory\n");
+      			deallocuvm(myproc()->pgdir, newsz, sz);
+      			return -1;
+    		}
+    		memset(mem, 0, PGSIZE);
+    		if(mappages(myproc()->pgdir, (char*)a, PGSIZE, V2P(mem), PTE_W|PTE_U) < 0){
+      			cprintf("allocuvm out of memory (2)\n");
+      			deallocuvm(myproc()->pgdir, newsz, sz);
+      			kfree(mem);
+      			return -1;
+    		}
+  	}
+	
+	*pointer = (char*)a;
+	
+	shm_table.shm_pages[i].id = maxid;
+	shm_table.shm_pages[i].frame = (char*)a;
+	shm_table.shm_pages[i].refcnt = 1; 
+	
+	myproc()->sz = newsz;
+  	release(&(shm_table.lock));
+	
+	
+	
+	return -1; //added to remove compiler warning -- you should decide what to return
 }
 
 
